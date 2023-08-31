5b01e5b134b19ddd16cb1ebab2f1ac274e4c40ea
ecmu <ecmu@users.noreply.github.com>
2023-08-31 15:47:02 +0200

    Update CreateNewRelease.sh

--- CreateNewRelease.sh
+++ CreateNewRelease.sh
@@ -2,9 +2,17 @@
 
 #printenv
 
-echo {"tag_name":"$remote_version","target_commitish":"main","name":"$remote_version","body":"New release of shopping list","draft":false,"prerelease":false,"generate_release_notes":false} >data.json
+cat <<EOF > data.json
+{
+  "tag_name":"$remote_version",
+  "target_commitish":"main",
+  "name":"$remote_version",
+  "body":"New release of shopping list",
+  "draft":false,
+  "prerelease":false,
+  "generate_release_notes":false
+}
+EOF
 cat data.json
 
-#curl -L -X POST -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" \
-# -H "Authorization: Bearer $GITHUB_TOKEN" https://api.github.com/repos/$GITHUB_REPOSITORY/releases \
-# -d '{"tag_name":"v1.0.0","target_commitish":"main","name":"v1.0.0","body":"Description of the release","draft":false,"prerelease":false,"generate_release_notes":false}'
+#curl -L -X POST -header "Accept: application/vnd.github+json" --header "X-GitHub-Api-Version: 2022-11-28" --header "Authorization: Bearer $GITHUB_TOKEN" --data @data.json https://api.github.com/repos/$GITHUB_REPOSITORY/releases
